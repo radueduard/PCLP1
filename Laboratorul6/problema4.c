31be94ecb6ce39b8480697f20b03ebd04ca417cb
Your Name <you@example.com>
2022-11-23 17:20:58 +0200

    Laboratorul6

--- Laboratorul6/problema4.c
+++ Laboratorul6/problema4.c
@@ -3,9 +3,26 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
+int compar(char v1[], char v2[])
+{
+    int i = 0, j = 0;
+    while (v1[i] & v2[j])
+    {
+        if (v1[i] > v2[j]) return 1;
+        if (v1[i] < v2[j]) return -1;
+        i++;
+        j++;
+    }
+    if (i == j) return 0;
+    if (v1[i]) return 1;
+    return -1;
+}
 int main()
 {
-    puts("Hello World!");
+    char v1[100], v2[100];
+    scanf("%s%s", v1, v2);
+    printf("%d", compar(v1, v2));
     return 0;
 }
