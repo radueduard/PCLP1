89726769349dcc80e857ee20e9fff0f1b69a8815
radueduard <eduard_andrei.radu@stud.acs.upb.ro>
2022-12-02 14:38:21 +0200

    Fixed

--- Laboratorul7/problema4.c
+++ Laboratorul7/problema4.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-
-char* substr(char *src, int n, char *dest)
-{
-    strcpy(src+n,"");
-    strcpy(dest,src);
-    printf("%s",dest);
-}
 
 int main()
 {
-    int p,n;
-    char s[100], d[100];
-    fgets(s,100,stdin);
-    scanf("%d%d",&p,&n);
-    substr(s+p,n,d);
+    printf("hello world\n");
     return 0;
 }
