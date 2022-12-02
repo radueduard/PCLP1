85fa72023b1083dea40b282bf3136fd4ab4f7640
radueduard <92775880+radueduard@users.noreply.github.com>
2022-12-02 15:54:45 +0200

    Update problema3.c

--- Laboratorul7/problema3.c
+++ Laboratorul7/problema3.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* strdel(char* p, int n);
+char* strins(char* p, char* s);
 
 int main()
 {
-    printf("Hello world\n");
-    return 0;
+    char text[5000], x[5000], y[5000];
+    scanf("%[^\n]s", text);
+    scanf("%s%s", x, y);
+    if (strlen(text) > 1000 || strlen(x) > 20 || strlen(y) > 20)
+    {
+        printf("Date invalid introduse.");
+        return 0;
+    }
+    char* t = NULL;
+    t = strstr(text, x);
+    while (t != NULL)
+    {
+        strdel(t, strlen(x));
+        strins(t, y);
+        t = strstr(t + strlen(y), x);
+    }
+    printf("%s\n", text);
+}
+
+char* strdel(char* p, int n)
+{
+    strcpy(p, p + n);
+    return p;
+}
+
+char* strins(char* p, char* s)
+{
+    char* q;
+    q = malloc(20 * sizeof(char));
+    strcpy(q, p);
+    strcpy(p, s);
+    strcat(p, q);
+    return p;
+    free(q);
 }
