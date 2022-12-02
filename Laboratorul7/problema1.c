9e3ff959f5bf9e3bb2a4433a5daebb73f8b217e2
radueduard <eduard_andrei.radu@stud.acs.upb.ro>
2022-12-02 14:38:21 +0200

    Fixed

--- Laboratorul7/problema1.c
+++ Laboratorul7/problema1.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
-void swap (int *pa, int *pb)
-{
-    int x;
-    x=*pa;
-    *pa=*pb;
-    *pb=x;
-}
-
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    swap(&a,&b);
-    printf("%d %d",a,b);
+    printf("Hello world\n");
     return 0;
 }
