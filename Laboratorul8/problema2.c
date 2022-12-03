89ee488ecea111d098d51e17215a0f09b700688c
Diaconu Alexandra Ioana <diaconu.alexandra.ioana@gmail.com>
2022-12-03 13:48:40 +0200

    Laboratorul 8

--- Laboratorul8/problema2.c
+++ Laboratorul8/problema2.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world\n");
-    return 0;
+    int *v, n = 0, cap = 5, x, i;
+    v = malloc(cap * sizeof(int));
+    scanf("%d", &x);
+    while (x != 0)
+    {
+        if (cap == n)
+        {
+            cap = cap * 2;
+            v = realloc(v, cap * sizeof(int));
+        }
+        v[n++] = x;
+        scanf("%d", &x);
+    }
+    for (i = 0; i < n; i++)
+        printf("%d ", v[i]);
+    printf("\n");
 }
--- Laboratorul8/problema3.c
+++ Laboratorul8/problema3.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world\n");
-    return 0;
+    int n, i, gasit;
+    char(*cuvinte)[20], (*cuv)[20] = NULL;
+    int *aparitii;
+    scanf("%d\n", &n);
+    cuvinte = malloc(n * sizeof(char[20]));
+    for (i = 0; i < n; i++)
+        scanf("%s", cuvinte[i]);
+    int dim = 4, nr = 0, j;
+    cuv = malloc(dim * sizeof(char[20]));
+    aparitii = malloc(dim * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        gasit = 0;
+        for (j = 0; j < nr; j++)
+            if (strcmp(cuv[j], cuvinte[i]) == 0)
+            {
+                aparitii[j]++;
+                gasit = 1;
+            }
+        if (gasit != 1)
+        {
+            if (nr >= dim)
+            {
+                dim = dim * 2;
+                aparitii = realloc(aparitii, dim * sizeof(int));
+                cuv = realloc(cuv, dim * sizeof(char[20]));
+            }
+            strcpy(cuv[nr], cuvinte[i]);
+            aparitii[nr] = 1;
+            nr++;
+        }
+    }
+    for (i = 0; i < nr; i++)
+        printf("%s %d\n", cuv[i], aparitii[i]);
 }
