b8eadb9ea28fad4643a349ec77bd1e5aa6968c48
sibel_grozea <sibel.grozea@gamil.com>
2022-12-03 12:06:26 +0200

    Laboratorul8

--- Laboratorul8/problema2.c
+++ Laboratorul8/problema2.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-    printf("Hello world\n");
+    int *v;
+    int cap=5,n,y=1;
+    v=malloc(cap*sizeof(int));
+    while(y!=0)
+    {
+        scanf("%d",&y);
+        if(y!=0)
+        {
+            v[n++]=y;
+        }
+        else
+            break;
+        if(n==cap)
+        {
+            cap++;
+            v=realloc(v,cap*sizeof(int));
+        }
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",v[i]);
     return 0;
 }
