f2433e5ccad1abaaeeeb69500f415e887ee9e223
ruxi <oana_diaconu&@stud.acs.upb.ro>
2022-12-03 12:27:15 +0200

    Laboratorul8

--- Laboratorul8/problema1.c
+++ Laboratorul8/problema1.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main()
-{
-    printf("Hello world\n");
-    return 0;
+char* my_replace(char *s, char *s1, char *s2){
+    char aux[101];
+    char *p;
+    p=strtok(s," ");
+    while(p){
+        if(strcmp(p,s1)==0)
+            strcat(aux,s2);
+        strcat(aux,p);
+        p=strtok(NULL," ");
+    }
+    strcpy(s,aux);
+}
+
+int main(){
+    char s1[101],s2[101],s[101];
+    fgets(s1,101,stdin);
+    fgets(s2,101,stdin);
+    fgets(s,101,stdin);
+    my_replace(s,s1,s2);
+    printf("%s",s);
 }
--- Laboratorul8/problema2.c
+++ Laboratorul8/problema2.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-int main()
-{
-    printf("Hello world\n");
-    return 0;
+int main(){
+    int *v, cap=5,n=0;
+    v=malloc(cap*sizeof(int));
+    int x;
+    scanf("%d",&x);
+    while(x){        
+        n++;
+        if(n==cap){
+            v=realloc(v,2*cap*sizeof(int));
+            cap = 2*cap;
+        }
+        v[n]=x;
+        scanf("%d",&x);
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",v[i]);
 }
