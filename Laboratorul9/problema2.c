f3e8d4f4d7f88fa217f02ffeb613aecf0f839040
Borromeu <adelin_andrei.fulop@stud.acs.upb.ro>
2022-12-08 13:57:34 +0200

    Laboratorul9/

--- Laboratorul9/problema2.c
+++ Laboratorul9/problema2.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-
-int main()
-{
-    printf("Hello world\n");
+#include <time.h>
+#include <stdlib.h>
+struct time { // definita in time.h
+    int tm_sec,tm_min, tm_hour; // sec (0-59), min (0-59), ora (0-23)
+    int tm_mday, tm_mon, tm_year; // zi (0-31), luna (0-11), an (dupa 1900)
+    int tm_wday; // ziua din saptamana (0-6)
+    int tm_yday; // ziua din an (0-365)
+    int tm_isdst; // +1 Daylight Savings Time, 0 No DST, -1 don't know
+}ora;
+int main() {
+    time_t t1;
+    time(&t1);
+    struct time t2 = *(struct time *)localtime(t1);
+    printf("%s %s", ctime(&t1), asctime(&t2));
     return 0;
 }
