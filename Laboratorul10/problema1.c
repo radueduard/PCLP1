febdd4afadab890ff2b22b10220ec01d2b181417
root <root@DESKTOP-2VTTKVO.localdomain>
2022-12-15 13:57:28 +0200

    Laboratorul10

--- Laboratorul10/problema1.c
+++ Laboratorul10/problema1.c
@@ -1,10 +1,20 @@
-//
-// Created by Eduard Andrei Radu on 09.11.2022.
-//
-
 #include <stdio.h>
+#include <math.h>
+double integrala(double (*func) (double x), double a, double b, int n) {
+double sum = 0, baza_mica, baza_mare;
+double pas = (a - b) / n;
+int i;
+for (i = 1; i < n; i++) {
+	baza_mica = pas * (i - 1);
+	baza_mare = pas * i;
+	sum += (func(baza_mica)+func(baza_mare)) * pas /2;
+}
+return sum;
+}
 
 int main() {
-	puts("Hello World!");
+	printf("Integrala din sin(x) de la o la PI = %2.f\n"), integrala(sin, 0, M_PI, 100);
+	printf("Intergala din cos(x) de la 0 la PI = %.2f\n"), integrala(cos, 0, M_PI, 100);
+	
 	return 0;
 }
