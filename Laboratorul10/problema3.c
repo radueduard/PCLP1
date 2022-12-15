8fabb7d3052ea791d079b6f4ee27d296655096c0
Vlad <vlad_mihai.stanciu@stud.acs.upb.ro>
2022-12-15 13:58:42 +0200

    Laboratorul10

--- Laboratorul10/problema3.c
+++ Laboratorul10/problema3.c
@@ -3,8 +3,22 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+struct MATERIA
+{
+	char nume[16];
+	int ore_curs;
+	char tip_examen[2];
+}
 
+void citire_MAT(struct MATERIA x)
+{
+	fgets(x,1000,stdin);
+	
+}
 int main() {
-	puts("Hello World!");
+
+	
 	return 0;
 }
--- Laboratorul10/problema4.c
+++ Laboratorul10/problema4.c
@@ -1,10 +1,82 @@
-//
-// Created by Eduard Andrei Radu on 09.11.2022.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+	int n;
+	int m;
+	int **a;
+} MATRICE;
 
-int main() {
-	puts("Hello World!");
+MATRICE *creeaza_MATRICE(int n, int m)
+{
+	MATRICE *matrix = malloc(sizeof(MATRICE));
+	matrix->n = n;
+	matrix->m = m;
+	matrix->a = (int **)malloc(n * sizeof(int *));
+	for (int i = 0; i < m; i++)
+		matrix->a[i] = (int *)malloc(m * sizeof(int));
+	return matrix;
+}
+
+MATRICE *citeste_MATRICE(MATRICE *ma)
+{
+	for (int i = 0; i < ma->n; i++)
+		for (int j = 0; j < ma->m; j++)
+			scanf("%d", &ma->a[i][j]);
+	return ma;
+}
+
+void scrie_MATRICE(MATRICE *ma)
+{
+	for (int i = 0; i < ma->n; i++)
+	{
+		for (int j = 0; j < ma->m; j++)
+			printf("%d ", ma->a[i][j]);
+		printf("\n");
+	}
+}
+
+MATRICE *aduna_MATRICE(MATRICE *ma, MATRICE *mb)
+{
+	if (ma->n != mb->n || ma->m != mb->m)
+		return NULL;
+	MATRICE *mc = creeaza_MATRICE(ma->n, ma->m);
+	for (int i = 0; i < ma->n; i++)
+		for (int j = 0; j < ma->m; j++)
+			mc->a[i][j] = ma->a[i][j] + mb->a[i][j];
+	return mc;
+}
+
+MATRICE *inmulteste_MATRICE(MATRICE *ma, MATRICE *mb)
+{
+	if (ma->m != mb->n)
+		return NULL;
+	MATRICE *mc = creeaza_MATRICE(ma->n, mb->m);
+	for (int i = 0; i < mc->n; i++)
+		for (int j = 0; j < mc->m; j++)
+		{
+			mc->a[i][j] = 0;
+			int row = i;
+			int col = j;
+			for (int k = 0; k < ma->m; k++)
+				mc->a[i][j] = mc->a[i][j] + ma->a[row][k] * mb->a[k][col];
+		}
+	return mc;
+}
+
+int main()
+{
+
+	int n_a, m_a, n_b, m_b;
+	scanf("%d%d", &n_a, &m_a);
+	MATRICE *ma = creeaza_MATRICE(n_a, m_a);
+	citeste_MATRICE(ma);
+	scanf("%d%d", &n_b, &m_b);
+	MATRICE *mb = creeaza_MATRICE(n_b, m_b);
+	citeste_MATRICE(mb);
+
+	MATRICE *md = inmulteste_MATRICE(ma, mb);
+	scrie_MATRICE(md);
+
 	return 0;
 }
