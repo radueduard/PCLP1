3f98cc817494b41b8ca1baa6ef93c9bf5aea2d92
sibel_grozea <sibel.grozea@gamil.com>
2022-12-15 13:59:07 +0200

    lab10

--- Laboratorul10/problema4.c
+++ Laboratorul10/problema4.c
@@ -3,7 +3,36 @@
 //
 
 #include <stdio.h>
+typedef struct{
+	int n;
+	int m;
+	int **a;
+}matrice;
 
+matrice* creeaza_MATRICE(int n, int m)
+{
+	matrice x;
+	x->a=(int **)malloc(n*sizeof(int*));
+	for(int i=0;i<n;i++)
+	   x->a[i]=(int *)malloc(m*sizeof(int));
+	return *x;
+}
+matrice* citeste_MATRICE(matrice *ma)
+{
+	for(int i=0;i<ma->n;i++)
+		for(int j=0;j<ma->m;j++)
+			scanf("%d",&ma->a[i][j]);
+	return *ma;
+}
+void scrie_MATRICE(matrice *ma)
+{
+	for(int i=0;i<ma->n;i++)
+		{
+			for(int j=0;j<ma->m;j++)
+				printf("%d ",a[i][j]);
+			printf("\n");
+		}
+}
 int main() {
 	puts("Hello World!");
 	return 0;
