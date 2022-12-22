4874999e96bb494d66942b7833dd9529735f239e
Your Name <you@example.com>
2022-12-22 15:56:16 +0200

    Lab11

--- Laboratorul11/problema3A.c
+++ Laboratorul11/problema3A.c
@@ -1,10 +1,24 @@
-//
-// Created by Eduard Andrei Radu on 09.11.2022.
-//
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-	puts("Hello World!");
-	return 0;
+void cautare(char *filename, char *str){
+    FILE *fp;
+    fp= fopen(filename,"r");
+    if (fp == NULL){
+        printf("Nu s-a putut deschide fisierul");
+    }
+    char s[100];
+    while(fgets(s,100,fp)!=NULL){
+        if(strstr(s,str)!=0){
+            printf("%s",s);
+        }
+    }
+    fclose(fp);
+}
+
+int main(char *argv[])
+{
+    cautare(argv[1],argv[2]);
+    return 0;
 }
