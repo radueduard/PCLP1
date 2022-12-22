dfdd9a87b1ed536f2ec032e67929c36623a8751f
Iudenco Ianos <ianosiudenco@stud.acs.upb.ro>
2022-12-22 16:28:21 +0200

    Lab11

--- Laboratorul11/problema3A.c
+++ Laboratorul11/problema3A.c
@@ -1,10 +1,22 @@
-//
-// Created by Eduard Andrei Radu on 09.11.2022.
-//
 
 #include <stdio.h>
+#include <string.h>
+#define MAX 1005
 
-int main() {
-	puts("Hello World!");
-	return 0;
+int main(int argc, char** argv) {
+
+    FILE * fisier;
+    char auxiliar[MAX];
+    fisier = fopen(argv[1], "r");
+
+    while(fgets(auxiliar, MAX, fisier) != NULL)
+    {
+        if(strstr(auxiliar, argv[2])) 
+		{
+            printf("%s\n", auxiliar);
+        }
+    }
+
+    fclose(fisier);
+    return 0;
 }
--- Laboratorul11/problema4.c
+++ Laboratorul11/problema4.c
@@ -1,10 +1,64 @@
-//
-// Created by Eduard Andrei Radu on 09.11.2022.
-//
-
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	puts("Hello World!");
-	return 0;
+int main(int argc, char **argv)
+{
+    char tabel[4];
+    char input[1005];
+	char tabel2[15][2];
+
+	int i, j = 0;
+    for(i = 0; i < 1005; i++)
+	{
+        input[i] = '\0';
+	}
+    FILE *fisier1, *fisier2, *fisier3;
+	
+    fisier1 = fopen(argv[1], "r");
+    fisier2 = fopen(argv[2], "r");
+    fisier3 = fopen(argv[3], "w");
+
+    while(fgets(tabel, 4, fisier1) != NULL)
+    {
+        tabel2[j][0] = tabel[0];
+        tabel2[j][1] = tabel[2];
+        j++;
+    }
+    while(fgets(input, 1005, fisier2) != NULL)
+    {
+        input[strlen(input) - 1] = '\0';
+        for (int i = 0; i < strlen(input); i++)
+        {
+            int counter = 0;
+            for (int j = 0; j < 15; j++)
+            {
+                if (input[i] == tabel2[j][0] || input[i] == tabel2[j][0] - 32)
+                {
+                    if (input[i] == tabel2[j][0])
+					{
+                        fputc(tabel2[j][1], fisier3);
+					}
+                    else
+					{
+                        fputc(tabel2[j][1] - 32, fisier3);
+					}
+                    counter = 1;
+                    break;
+                }
+            }
+            if (counter == 0)
+            {
+                fputc(input[i], fisier3);
+            }
+        }
+		
+        fputc('\n', fisier3);
+        for (int i = 0; i < 1005; i++)
+		{
+            input[i] = '\0';
+		}
+    }
+
+    fclose(fisier2);  fclose(fisier3); fclose(fisier1);
+    return 0;
 }
