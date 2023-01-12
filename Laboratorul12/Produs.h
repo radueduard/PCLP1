2f7f825018ed1c4a6f8d15431c19b69155327bda
Stiuj Emanuel <emanuel.stiuj@stud.acs.upb.ro>
2023-01-12 13:48:50 +0200

    Laboratorul12

---
+++ Laboratorul12/Produs.h
@@ -0,0 +1,5 @@
+typedef struct {
+    char nume_produs[20];
+    int cantitate;
+    double pret_produs;
+}Produs;
--- Laboratorul12/problema1.c
+++ Laboratorul12/problema1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "Produs.h"
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-	puts("Hello World!");
+int main(int argc, char *argv[]) {
+
+    Produs produs;
+    strcpy(produs.nume_produs, argv[1]);
+    produs.cantitate = atoi(argv[2]);
+    produs.pret_produs = atof(argv[3]);
+
+    printf("%s %d %lf", produs.nume_produs, produs.cantitate, produs.pret_produs);
 	return 0;
 }
