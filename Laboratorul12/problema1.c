4a87081cd879430feb1e34f890d32d515bac01e1
Your Name <you@example.com>
2023-01-12 14:02:15 +0200

    Lab12

--- Laboratorul12/problema1.c
+++ Laboratorul12/problema1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "Produs.h"
 
-int main() {
-	puts("Hello World!");
-	return 0;
+int main(int argc, char *argv[])
+{
+    Produs produs;
+    sscanf(argv[1], "%s", produs.nume_produs);
+    sscanf(argv[2], "%d", &produs.cantitate);
+    sscanf(argv[3], "%f", &produs.pret_produs);
+
+    printf("Produsul %s, cantitatea %d, pretul %.2f\n", produs.nume_produs, produs.cantitate, produs.pret_produs);
+    return 0;
 }
